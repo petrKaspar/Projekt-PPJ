b834515716c8d9d722e97f50e9229bf97e2f38df
petrKaspar <petr1kaspar@gmail.com>
2016-06-11 01:12:04 +0200

    pridani base repozitory a entity

---
+++ src/main/java/cz/tul/data/Tag.java
@@ -0,0 +1,7 @@
+package cz.tul.data;
+
+/**
+ * Created by Petr on 11.06.2016.
+ */
+public class Tag {
+}
---
+++ src/main/java/cz/tul/repositories/BaseKomentarRepository.java
@@ -0,0 +1,7 @@
+package cz.tul.repositories;
+
+/**
+ * Created by Petr on 11.06.2016.
+ */
+public class BaseKomentarRepository {
+}
